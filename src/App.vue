5617fc908d2a6af1f1ebf684a4d0552387a6ff68
stephane888 <kksasteph888@gmail.com>
2021-10-22 03:42:35 +0200

    MAJ

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
   <div id="app" class="app-form">
-    <div id="nav" v-if="$store.state.mode">
-      <!-- <router-link to="/"> Gestion des formlaires </router-link> |
-      <router-link to="/about">About</router-link> -->
-    </div>
+    <div id="nav" v-if="$store.state.mode"></div>
     <router-view />
   </div>
 </template>
 <script>
 export default {
   name: "App",
-  mounted() {
-    this.$store.dispatch("loadStepsDatas");
-  },
+  // mounted() {
+  //   //this.$store.dispatch("loadStepsDatas");
+  // },
 };
 </script>
-<style lang="scss">
-#nav {
-  padding: 30px;
-  text-align: center;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>

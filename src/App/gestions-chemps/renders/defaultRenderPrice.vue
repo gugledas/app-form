8527367d27a00a0cd0370ae3911148273df826f6
stephane888 <kksasteph888@gmail.com>
2021-11-27 18:14:06 +0100

    MAJ

---
+++ src/App/gestions-chemps/renders/defaultRenderPrice.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b v-if="field.prix && parseFloat(field.prix.cout)">
+      {{ field.prix.cout }} €
+    </b>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  name: "radio",
+  props: {
+    field: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
